554fafb9e7095f4bf24a04967cf72ef04db6f918
João Vieira <j.vieira.41@hotmail.com>
2021-01-01 16:33:33 +0000

    update 12

--- src/urls.py
+++ src/urls.py
@@ -17,17 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import startpage_view, login_view, login_attempt, home_view
+from .views import home_view
 
 urlpatterns = [
-    path('home/', home_view, name="home"),
     path('profiles/', include('perfil.urls', namespace='perfil')),
-    path('', startpage_view, name='startpage'),
-    path('login', login_view, name="login"),
-    path('login_attempt', login_attempt, name="login_attempt"),
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
