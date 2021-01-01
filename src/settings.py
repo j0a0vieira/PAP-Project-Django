dbb6a4fcfdb1c3390c3d8d8bfb7cdfb31f757314
João Vieira <j.vieira.41@hotmail.com>
2021-01-01 18:12:34 +0000

    god save me

--- src/settings.py
+++ src/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.djangi.GzipManifestStaticFilesStorage'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
