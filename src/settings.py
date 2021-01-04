a161abb2e5108a7c1f37c96f48d26e82dc9daadf
João Vieira <j.vieira.41@hotmail.com>
2021-01-04 07:11:24 +0000

    update session

--- src/settings.py
+++ src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'df8hsxybzcgr11_r5kz$co_&(g*+yit4al$40tp!xpi@2tlm5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
