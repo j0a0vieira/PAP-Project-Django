d11da634adb85f15ffd252eeb6c06eb95298bc62
João Vieira <j.vieira.41@hotmail.com>
2021-01-14 18:53:06 +0000

    14/01

---
+++ staticfiles/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.update_account').click(function(){
+        $('.ui.modal.update').modal('show');
+    })
+
+    $('#friend_list_popup').click(function(){
+        $('.ui.modal.friendlist').modal('show');
+    })
+})
