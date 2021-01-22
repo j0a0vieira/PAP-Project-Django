4b7216fa2e324b93e518cc8c009ef5963398bc7c
João Vieira <j.vieira.41@hotmail.com>
2021-01-22 22:03:46 +0000

    posts

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import Post, Like
 from perfil.models import Profile
+from .forms import PostModelForm
 
 # Create your views here.
 def post_comment_create_and_list_view(request):
     qs = Post.objects.all()
     profile = Profile.objects.get(username=request.user)
 
+    #Post form, comment form
+    p_form = PostModelForm(request.POST or None, request.FILES or None)
+    profile = Profile.objects.get(username = request.user) #ir buscar o user logado para lhe dar assign ao post
+
+    if p_form.is_valid():
+        instance = p_form.save(commit=False)
+        instance.author = profile
+        instance.save()
+        p_form = PostModelForm()
+
     context = {
         'qs': qs,
         'profile': profile,
+        'p_form': p_form,
     }
 
     return render(request, 'posts/main.html', context)
