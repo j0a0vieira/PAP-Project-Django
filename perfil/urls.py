89b178bb491cd0841e33c3f08678006fc7e30f79
João Vieira <j.vieira.41@hotmail.com>
2021-02-03 00:36:46 +0000

    template pedidos de amizade

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view
+from .views import ( 
+        my_profile_view, 
+        invites_received_view, 
+        invite_profiles_list_view, 
+        ProfileListView 
+)
 
 app_name = 'perfil'
 
 urlpatterns = [
     path('myprofile/', my_profile_view, name="my-profile-view"),
     path('my_invites/', invites_received_view, name="my-invites-view"),
-    path('all_profiles/', profiles_list_view, name="all-profiles-view"),
+    path('all_profiles/', ProfileListView.as_view(), name="all-profiles-view"),
     path('to_invite/', invite_profiles_list_view, name="invite-profiles-view"),
 ]
