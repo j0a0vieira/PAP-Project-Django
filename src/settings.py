e4e2315cc6701ed69e6b0e71fea2a7f09c0ba1d8
João Vieira <j.vieira.41@hotmail.com>
2021-02-04 15:57:59 +0000

    added allowed hosts

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'df8hsxybzcgr11_r5kz$co_&(g*+yit4al$40tp!xpi@2tlm5g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://codelize.herokuapp.com/', '127.0.0.1:8080']
 
 
 # Application definition
