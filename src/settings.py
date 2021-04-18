e84bcb34db88a740fa92d6953cc1fc8e7295ced3
vieiraCoder <vieira.v1551@gmail.com>
2021-04-18 13:38:15 +0100

    bug fix

--- src/settings.py
+++ src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'df8hsxybzcgr11_r5kz$co_&(g*+yit4al$40tp!xpi@2tlm5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
