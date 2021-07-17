9d1bdd90524edf2d2c659a3cec11a978aea95bb1
vieiraCoder <vieira.v1551@gmail.com>
2021-07-17 20:37:07 +0100

    final

--- src/views.py
+++ src/views.py
@@ -2,7 +2,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from perfil.models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
